45049851c75cfe31ac82d5ac86d603f41431eaa6
Deepak Vadithala <dv@deepaks-imac.home>
2018-02-03 23:03:03 +0000

    Added tokenized words for each course and stored it as a dictionary

--- Courses_Tokenization.py
+++ Courses_Tokenization.py
@@ -1,6 +1,18 @@
 import pandas as pd
 import numpy as np
+from nltk import word_tokenize
 from collections import Counter
 
+# Read the CSV file as Pandas Dataframe
 my_courses = pd.read_csv('./Data/Courses.csv')
-print(type(my_courses))
+
+my_courses_tokenized = {}
+
+for index, row in my_courses.iterrows():
+    try:
+        my_courses_tokenized.update({ row['Course Id'] : word_tokenize(row['Course Description']) })
+    except:
+        pass
+
+print(my_courses_tokenized)
+
